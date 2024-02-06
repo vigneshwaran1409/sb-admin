f8b207fdeae79c220e9a57cf63e6c81352b8a195
vigneshwaran1409 <mvigneshwaran1409@gmail.com>
2024-02-06 08:33:25 +0530

    react sb-admin

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './assets/Components/Sidebar'
+import Dashboard from './assets/Components/Dashboard'
+function App() {
+  return <div id="wrapper">
+         <Sidebar/>
+         <Dashboard/>
+  </div>
+}
+
+export default App
